429005d9bf2a0ad83c81d3a8d6409ab7943d9860
Fernando <64383713+ffeernando@users.noreply.github.com>
2025-07-23 14:11:40 -0600

    Create Problemas Rentabilidad Ejemplo Pulp.py

---
+++ Problemas Rentabilidad Ejemplo Pulp.py
@@ -0,0 +1,57 @@
+# Problema de Asignación de Visitas
+
+### Importamos librería
+import pulp
+
+### Constantes y parámetros del problema
+# Grupos
+grupos = ['G1', 'G2', 'G3', 'G4']
+
+# Número máximo de visitas por cliente
+k_max = 8  
+
+# Total de visitas disponibles 
+total_visitas = 2200
+
+# Número de clientes por grupo
+poblaciones = {'G1': 280, 'G2': 240, 'G3': 240, 'G4': 240}
+
+# Rentabilidad
+r_g = {
+    ('G1', 1): 467, ('G1', 2): 210, ('G1', 3): 99, ('G1', 4): 47, ('G1', 5): 22, ('G1', 6): 4, ('G1', 7): -2, ('G1', 8): -2,
+    ('G2', 1): 521, ('G2', 2): 241, ('G2', 3): 114, ('G2', 4): 59, ('G2', 5): 34, ('G2', 6): 13, ('G2', 7): 3, ('G2', 8): 5,
+    ('G3', 1): 533, ('G3', 2): 238, ('G3', 3): 114, ('G3', 4): 59, ('G3', 5): 34, ('G3', 6): 12, ('G3', 7): 4, ('G3', 8): 3,
+    ('G4', 1): 521, ('G4', 2): 238, ('G4', 3): 115, ('G4', 4): 58, ('G4', 5): 33, ('G4', 6): 14, ('G4', 7): 3, ('G4', 8): 6
+}
+
+### Creación del Solver
+prob = pulp.LpProblem("Maximizar Rentabilidad", pulp.LpMaximize)
+
+### Creación de variables x_g,k
+x = pulp.LpVariable.dicts("x", ((g, k) for g in grupos for k in range(1, k_max + 1)), lowBound=0, cat='Integer')
+
+### Función objetivo
+prob += pulp.lpSum(r_g[g, k] * x[g, k] for g in grupos for k in range(1, k_max + 1)), "Rentabilidad Total"
+
+### Restricción del total de visitas
+prob += pulp.lpSum(k * x[g, k] for g in grupos for k in range(1, k_max + 1)) == total_visitas, "Total Visitas"
+
+### Restriccion de visitas minimas
+for g in grupos:
+    prob += pulp.lpSum(x[g, k] for k in range(1, k_max + 1)) == poblaciones[g], f"Población_{g}"
+
+### Resolucion del problema
+prob.solve()
+
+
+### Verificación del estado de la solución
+if pulp.LpStatus[prob.status] == 'Optimal':
+    print("Solución Óptima Encontrada:")
+    for g in grupos:
+        for k in range(1, k_max + 1):
+            print(f"Visitas al grupo {g}, {k}-ésima visita: {x[g, k].varValue}")
+else:
+    print("No se encontró una solución óptima.")
+
+
+
